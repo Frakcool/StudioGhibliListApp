bf3b0f1e9ef9b978d2ca51230c4f6377b1b970e5
Frakcool <frakcool1993@gmail.com>
2019-02-19 22:06:24 -0500

    Started working with DetailsView in programmatic mode

---
+++ StudioGhibliListApp/View/DetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsViewController.swift
+//  StudioGhibliListApp
+//
+//  Created by Frakcool on 2/19/19.
+//  Copyright © 2019 Frakcool. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setupUI() {
+        self.view.backgroundColor = UIColor.white
+    }
+}
